6b1b5afc6e66c8ddb7311bf50b69b6ab3dfc2e21
shaneball85 <shaneball@u.boisestate.edu>
2023-11-26 16:37:08 -0700

    Add Constructors Getters and Setters

--- src/ParseObject.java
+++ src/ParseObject.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class ParseObject {
+public class ParseObject {
+    private int numberOfStates;
+    private int sigma;
+    private Transition[] transitions;
+    private String inputString;
+
+    public ParseObject(int numberOfTransitions) {
+        numberOfStates = 0;
+        sigma = 0;
+        transitions = new Transition[numberOfTransitions];
+        inputString = "";
+    }
+
+    public int getNumberOfStates() {
+        return numberOfStates;
+    }
+
+    public void setNumberOfStates(int numberOfStates) {
+        this.numberOfStates = numberOfStates;
+    }
+
+    public int getSigma() {
+        return sigma;
+    }
+
+    public void setSigma(int sigma) {
+        this.sigma = sigma;
+    }
+
+    public Transition[] getTransitions() {
+        return transitions;
+    }
+
+    public Transition getTransitionAtIndex(int index) {
+        return transitions[index];
+    }
+
+    public void setTransitions(Transition[] transitions) {
+        this.transitions = transitions;
+    }
+
+    public void addTransition(Transition transition) {
+        for (int i = 0; i < transitions.length; i++) {
+            if (transitions[i] == null) {
+                transitions[i] = transition;
+                return;
+            }
+        }
+    }
+
+    public String getInputString() {
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
 }
--- src/Transition.java
+++ src/Transition.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Transition {
+/**
+ * Author: Shane Ball
+ * CS 361 Fall 2023
+ * Project 3
+ *
+ * This class is part of the TM package. The transition object holds all the necessary information for one possible
+ * transition of the turing machine. These are built one by one as the given txt file is parsed.
+ */
+public class Transition {
+    private int nextState;
+    private int writeSymbol;
+    private char move;
+
+    public Transition(int next, int write, char move) {
+        this.nextState = next;
+        this.writeSymbol = write;
+        this.move = move;
+    }
+
+    public int getNextState() {
+        return nextState;
+    }
+
+    public void setNextState(int nextState) {
+        this.nextState = nextState;
+    }
+
+    public int getWriteSymbol() {
+        return writeSymbol;
+    }
+
+    public void setWriteSymbol(int writeSymbol) {
+        this.writeSymbol = writeSymbol;
+    }
+
+    public char getMove() {
+        return move;
+    }
+
+    public void setMove(char move) {
+        this.move = move;
+    }
 }
