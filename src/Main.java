dbd745160b0246938fdcb9521b9b27846d8cb319
shaneball85 <shaneball@u.boisestate.edu>
2023-11-26 19:04:55 -0700

    Main.java used for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        FileParser test = new FileParser("/Users/shaneball/Desktop/CS361/Projects/Project 3/test/file0.txt");
+
+        ParseObject textParse = test.parseFile();
+
+        System.out.println(textParse.toString());
     }
 }
