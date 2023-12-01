dfa1acd4499b5ba55e9eea5261346a1006ad3d7f
shaneball85 <shaneball@u.boisestate.edu>
2023-12-01 10:11:15 -0700

    Updated State initializer. Still getting errors with testing.

--- src/tm/TouringMachine.java
+++ src/tm/TouringMachine.java
@@ -15,45 +15,58 @@ public class TouringMachine {
     public TouringMachine(ParseObject parseObject) {
         this.inputString = parseObject.getInputString();
         tape = new LinkedList<>();
+        // If there is an input string, iterate through the string and add each character in the string one by one
+        // into the linked list as an integer
         if (inputString.length() > 0) {
             for (int i = 0; i < inputString.length(); i++) {
                 tape.add(i, Character.getNumericValue(inputString.charAt(i)));
             }
-            System.out.println("Tape of " + inputString + " Added To Machine");
+            System.out.println("Tape of " + inputString + " Added To Machine"); // Remove after testing complete
+        // If the input string from the file is blank, add a 0 into the head index of the linked list to represent
+        // the blank
         } else {
             tape.add(0,0);
-            System.out.println("Blank Tape Added To Machine");
+            System.out.println("Blank Tape Added To Machine"); // Remove after testing complete
         }
         headPointer = 0;
         statePointer = 0;
         this.numberOfStates = parseObject.getNumberOfStates();
         this.sigmaSize = parseObject.getSigma();
-        states = new State[numberOfStates];
+        states = new State[numberOfStates]; // Initialize the state array to the correct size for the machine
 
+        // Get the total number of transitions from the file to calculate the number of transitions that should be
+        // in each state
         int numberOfTransitions = parseObject.getTransitions().length;
-        int transitionsPerState = (numberOfStates - 1) / numberOfTransitions;
-        int transitionsIndex = 0;
+        // Calculate the number of transitions per state considering that the final state will not have any transitions
+        int transitionsPerState = numberOfTransitions / (numberOfStates - 1);
+        System.out.println("Transitions Per State = " + transitionsPerState); // Remove after testing complete
+        // start and end index used for the parseObject getTransitionsInRange() method to get the specified
+        // transitions subset
+        int startIndex = 0;
+        int endIndex = transitionsPerState - 1;
 
-        System.out.println("Starting to create states and add transitions...");
+        System.out.println("Starting to create states and add transitions..."); // Remove after testing complete
 
         for (int i = 0; i < states.length; i++) {
-            // Determine the number of transitions for the current state
-            int currentTransitionsCount = (i < states.length - 1) ? transitionsPerState : 0;
-            states[i] = new State(currentTransitionsCount);
-            System.out.println("State " + i + " created with " + currentTransitionsCount + " transitions");
-
-            /**
-             * This loop is currently not being reached to add the transitions to the state...
-             */
-            for (int j = 0; j < currentTransitionsCount; j++) {
-                int transitionIndex = i * transitionsPerState + j;
-                if (transitionIndex < parseObject.getTransitions().length) {
-                    states[i].setTransitionAtIndex(j, parseObject.getTransitionAtIndex(transitionIndex));
-                    System.out.println("Transition index = " + transitionIndex + " added to State " + i);
-                }
+            // Initialize the final state as a final state
+            if (i == states.length - 1) {
+                states[i] = new State(0);
+                System.out.println("Halt state created at States index " + i); // Remove after testing complete
+            // If it is not a final state initialize the state with the correct subset of transitions that belong to
+            // that state
+            } else {
+                // get the subset of transitions based on starting and ending index
+                Transition[] transitions = parseObject.getTransitionsInRange(startIndex, endIndex);
+                states[i] = new State(transitions);
+                System.out.println("State added to States index " + i); // Remove after testing complete
+                System.out.println(states[i]); // Remove after testing complete
+                startIndex += transitionsPerState; // Increment the start index to the range of the next state
+                System.out.println("Start Index = " + startIndex); // Remove after testing complete
+                endIndex += transitionsPerState; // Increment the end index to the range of the next state
+                System.out.println("End Index = " + endIndex); // Remove after testing complete
             }
         }
-        System.out.println("Machine created");
+        System.out.println("Machine created"); // Remove after testing complete
     }
 
 
