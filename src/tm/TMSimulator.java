90805958345da0b170704a6078a9e066f7a5cd50
Austin Andersen <awesomeaustinandersen@yahoo.com>
2023-12-02 15:29:05 -0700

    Update TMSimulator.java

--- src/tm/TMSimulator.java
+++ src/tm/TMSimulator.java
@@ -1,4 +1,17 @@
 package tm;
 
 public class TMSimulator {
+
+  String fileName = args[0];
+
+        FileParser test = new FileParser(fileName);
+
+        ParseObject textParse = test.parseFile();
+
+        System.out.println(textParse);
+
+        TouringMachine file0Machine = new TouringMachine(textParse);
+
+        System.out.println(file0Machine);
+  
 }
