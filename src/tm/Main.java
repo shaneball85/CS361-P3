9582e5e3b9c99e7b321e6ef4bb4fb321b87fe22b
shaneball85 <shaneball@u.boisestate.edu>
2023-12-02 14:24:12 -0700

    Updated Main for all the test outputs

--- src/tm/Main.java
+++ src/tm/Main.java
@@ -2,10 +2,16 @@ package tm;
 
 public class Main {
     public static void main(String[] args) {
-        FileParser test = new FileParser("/Users/shaneball/Desktop/CS361/Projects/Project 3/test/file0.txt");
+        FileParser test = new FileParser("/Users/shaneball/Desktop/CS361/Projects/Project 3/test/file5.txt");
         ParseObject textParse = test.parseFile();
         System.out.println(textParse);
         TouringMachine file0Machine = new TouringMachine(textParse);
         System.out.println(file0Machine);
+        while (file0Machine.getStatePointer() < (file0Machine.getNumberOfStates() - 1)) {
+            file0Machine.takeTransition();
+        }
+        System.out.println(file0Machine);
+        System.out.println("output length: " + file0Machine.getTape().tapeLength());
+        System.out.println("sum of symbols: " + file0Machine.getTape().tapeSum());
     }
 }
