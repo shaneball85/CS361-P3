f05399d2e95784ee4d75ba3de3db82f9932018d2
shaneball85 <shaneball@u.boisestate.edu>
2023-12-02 10:10:52 -0700

    Fixed bug in State toString()

--- src/tm/Main.java
+++ src/tm/Main.java
@@ -2,17 +2,10 @@ package tm;
 
 public class Main {
     public static void main(String[] args) {
-
         FileParser test = new FileParser("/Users/shaneball/Desktop/CS361/Projects/Project 3/test/file0.txt");
-
         ParseObject textParse = test.parseFile();
-
         System.out.println(textParse);
-
         TouringMachine file0Machine = new TouringMachine(textParse);
-
-        System.out.println("Entering the machine's toString");
-
         System.out.println(file0Machine);
     }
 }
